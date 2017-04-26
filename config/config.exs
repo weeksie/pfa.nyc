d6a7a7b4a0f56dbb1742a66706e6be278af23c3a
Scotty Weeks <scott.weeks@gmail.com>
2017-04-26 15:09:28 -0400

    more config thrashing

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,7 @@ config :pfa,
 # Configures the endpoint
 config :pfa, PFA.Endpoint,
   http: [port: { :system, "PORT" }],
-  url: [host: "0.0.0.0", port: { :system, "PORT" }],
+  url: [host: "0.0.0.0"],
   secret_key_base: "7w9mb2ly002WhDcD7yZlBIRORslkhzilRksyPsCFKDKlldsI4zRwx3MAseTfoiCT",
   render_errors: [view: PFA.ErrorView, accepts: ~w(html json)],
   pubsub: [name: PFA.PubSub,
